d9390e4267365a1620c35881566410c3dbd495c5
ezr1234 <enzo.z.rodrigues@gmail.com>
2019-09-02 12:39:44 -0300

    Table products on painel

--- src/views/examples/Painel.jsx
+++ src/views/examples/Painel.jsx
@@ -6,6 +6,8 @@ import {Redirect} from 'react-router-dom';
 
 import {
     Table,
+    Row,
+    Container
 }
 from 'reactstrap';
 
@@ -13,12 +15,32 @@ from 'reactstrap';
 
 require('../../assets/css/painel.css');
 
+let getProducts = async () =>{
+    var responseJson;
+    try {
+      let response = await fetch('http://brinquedox.herokuapp.com/allproducts', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+    });
+      responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  let idProduto = 0;
+
 export default class examples extends Component {
     constructor(props){
         super(props);
         this.newProduct = this.newProduct.bind(this);
         this.state = {
             redirect:false,
+            products:[]
         }
         
     }
@@ -26,6 +48,32 @@ export default class examples extends Component {
     newProduct(){
         return <Redirect to='/painel/newproduto'/>
     }
+
+    async componentDidMount(){
+        await getProducts().then((element) => {
+          const result = [];
+          for(let i in element){
+            var newProduct = {
+              atributos:{
+                nome:element[i].nome,
+                marca:element[i].marca,
+                valor:element[i].valor,
+                categoria:element[i].categoria
+              },
+              key: idProduto++
+            }
+            result.push(newProduct);
+          }
+          console.log(result)
+          this.setState({
+            products: result
+          })
+        })
+        .catch((error) => {
+          console.error('Opa! Houve um erro:', error.message);
+        });
+    
+      }
 
   render() {
     return (
@@ -46,6 +94,33 @@ export default class examples extends Component {
             </tr>
             </tbody>
         </Table>
+        <Container>
+            <Row>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <td>Nome</td>
+                                <td>Categoria</td>
+                                <td>Marca</td>
+                                <td>Preço</td>
+                                <td>Excluir</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.products.map((values)=>(
+                                <tr key={values.key}>
+                                <td>{values.atributos.nome}</td>
+                                <td>{values.atributos.categoria}</td>
+                                <td>{values.atributos.marca}</td>
+                                <td>R$: {values.atributos.valor}</td>
+                                <td>Excluir</td>
+                            </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+
+            </Row>
+        </Container>
         <Footer/>
         </>
     );
